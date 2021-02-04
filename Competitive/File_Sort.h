0cbbcda632dc9b14ad7bb7c945af77285fb4a423
Mayuresh <mmayuresh12@gmail.com>
2021-02-04 23:02:31 +0530

    Organized

---
+++ Competitive/File_Sort.h
@@ -0,0 +1,1 @@
+void file_even_odd_sort();
---
+++ Competitive/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "File_Sort.h"
+
+using namespace std;
+
+int main() {
+
+	/* Even and Odd numbers sorting & storing it in file */
+	file_even_odd_sort();
+
+	return 0;
+}
